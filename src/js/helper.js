1407b4fb1739a491fd11431ec699ad090f5e24b1
LilJack118 <78555188+LilJack118@users.noreply.github.com>
2021-09-05 21:36:36 +0200

    Update helper.js

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,7 +1,7 @@
 export const GetCountryData = async function () {
   try {
     const request = fetch(
-      "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/global_corona_actual_widok2/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true"
+      "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_corona_actual_widok2/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true"
     );
 
     const data = await request.then((response) => response.json());
@@ -15,7 +15,7 @@ export const GetCountryData = async function () {
 export const GetWojData = async function () {
   try {
     const request = fetch(
-      "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/wojewodztwa_corona_widok/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&outFields=*&sqlFormat=standard&orderByFields=jpt_nazwa_%20DESC&groupByFieldsForStatistics=jpt_nazwa_&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22potwierdzone_dzienne%22%2C%22outStatisticFieldName%22%3A%22potwierdzone_dzienne_sum%22%7D%5D"
+      "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/wojewodztwa_corona_widok/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&returnDistinctValues=false&returnIdsOnly=false&returnCountOnly=false&outFields=*&sqlFormat=standard&orderByFields=jpt_nazwa_%20DESC&groupByFieldsForStatistics=jpt_nazwa_&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22potwierdzone_dzienne%22%2C%22outStatisticFieldName%22%3A%22potwierdzone_dzienne_sum%22%7D%5D"
     );
 
     const data = await request.then((response) => response.json());
@@ -29,7 +29,7 @@ export const GetWojData = async function () {
 export const GetPowiatData = async function () {
   try {
     const request = fetch(
-      "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/powiaty_corona_widok_woj/FeatureServer/0/query?f=json&where=1%3D1&outFields=*&resultRecordCount=400"
+      "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/powiaty_corona_widok_woj/FeatureServer/0/query?f=json&where=1%3D1&outFields=*&resultRecordCount=400"
     );
     const data = await request.then((response) => response.json());
     return data;
